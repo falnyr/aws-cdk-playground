d712933132740d3bfd0d8d8a0da1f1d14c4dcecc
Jan Alfred Richter <falnyr@gmail.com>
2020-12-26 21:40:29 +1300

    feat: getting closer to cdk implementation.

--- lib/cdk-playground-stack.ts
+++ lib/cdk-playground-stack.ts
@@ -7,8 +7,12 @@ import {
   HttpRouteIntegrationConfig,
   HttpRouteKey,
   IHttpRouteIntegration,
-  PayloadFormatVersion
+  PayloadFormatVersion,
+  VpcLink
 } from '@aws-cdk/aws-apigatewayv2';
+import {DefaultInstanceTenancy, SecurityGroup, Vpc} from "@aws-cdk/aws-ec2";
+import {ApplicationLoadBalancer, ListenerAction} from "@aws-cdk/aws-elasticloadbalancingv2";
+import {HttpAlbIntegration, HttpProxyIntegration} from "@aws-cdk/aws-apigatewayv2-integrations";
 
 export class CdkPlaygroundStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -18,19 +22,51 @@ export class CdkPlaygroundStack extends cdk.Stack {
 
     const route = new HttpRoute(this, 'route1', {
       httpApi: api,
-      integration: new DummyIntegration(),
+      integration: new HttpProxyIntegration({
+        url: "https://http.cat/404",
+        method: HttpMethod.GET
+      }),
       routeKey: HttpRouteKey.with("/404", HttpMethod.GET),
     })
-  }
-}
 
-class DummyIntegration implements IHttpRouteIntegration {
-  public bind(): HttpRouteIntegrationConfig {
-    return {
-      type: HttpIntegrationType.HTTP_PROXY,
-      payloadFormatVersion: PayloadFormatVersion.VERSION_1_0,
-      uri: 'https://http.cat/404',
-      method: HttpMethod.GET,
-    };
+    const vpc = new Vpc(this, 'VPC1', {
+      cidr: '10.10.0.0/16',
+      enableDnsSupport: true,
+      enableDnsHostnames: true,
+      defaultInstanceTenancy: DefaultInstanceTenancy.DEFAULT
+    });
+
+    const sg1 = new SecurityGroup(this, 'SG1', { vpc });
+
+    const vpcLink = new VpcLink(this, 'VpcLink', { vpc });
+    vpcLink.addSubnets(...vpc.publicSubnets)
+    vpcLink.addSecurityGroups(sg1)
+
+    const lb = new ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true
+    });
+
+    const listener = lb.addListener('Listener', {
+      port: 80,
+      open: true,
+    });
+
+    listener.addAction('a1', {
+      action: ListenerAction.fixedResponse(200, {
+        messageBody: 'foobar',
+        contentType: 'text/plain',
+      })
+    })
+
+    const route2 = new HttpRoute(this, 'route2', {
+      httpApi: api,
+      integration: new HttpAlbIntegration({
+        listener: listener,
+        vpcLink: vpcLink,
+      }),
+      routeKey: HttpRouteKey.with("/404", HttpMethod.GET),
+    })
+
   }
 }
--- src/fileUploaded.ts
+++
@@ -1,7 +0,0 @@
-import {
-    APIGat
-}
-
-export const handler = async (
-    event:
-)
